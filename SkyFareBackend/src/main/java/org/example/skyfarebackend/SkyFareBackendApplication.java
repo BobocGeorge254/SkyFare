f8368d9590b93fb570cc279ec86ef54160482f21
Nicolae Dirva <99103798+NicuDirva@users.noreply.github.com>
2025-09-06 00:16:12 +0300

    init projects

---
+++ SkyFareBackend/src/main/java/org/example/skyfarebackend/SkyFareBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.skyfarebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyFareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyFareBackendApplication.class, args);
+	}
+
+}
---
+++ SkyFareBackend/src/test/java/org/example/skyfarebackend/SkyFareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skyfarebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyFareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
